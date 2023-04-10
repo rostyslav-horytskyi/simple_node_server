0cf57c2705d1c0d33c093f7c210a3895d86f4059
Rostyslav Horytskyi <gorzer.rostyslav@gmail.com>
2023-04-10 13:57:39 +0300

    add axios

--- request.js
+++ request.js
@@ -1,23 +1,33 @@
 const http = require("http");
+const axios = require("axios");
 
-// configure a request
-const req = http.request("http://localhost:3000", (res) => {
-  console.log(res.statusCode);
+axios
+  .get("http://localhost:3000")
+  .then((res) => {
+    console.log(res.status);
 
-  res.setEncoding("utf8");
+    console.log(res.data);
+  })
+  .catch((error) => console.error(error));
 
-  // subscribes on receiving a portion of response data
-  res.on("data", (data) => {
-    // process a portion of a response data here
-    console.log(data);
-  });
-});
+// // configure a request
+// const req = http.request("http://localhost:3000", (res) => {
+//   console.log(res.statusCode);
 
-// subscribes on any request error
-req.on("error", (error) => {
-  // handle an error here
-  console.error(error);
-});
+//   res.setEncoding("utf8");
 
-// sends the request
-req.end();
+//   // subscribes on receiving a portion of response data
+//   res.on("data", (data) => {
+//     // process a portion of a response data here
+//     console.log(data);
+//   });
+// });
+
+// // subscribes on any request error
+// req.on("error", (error) => {
+//   // handle an error here
+//   console.error(error);
+// });
+
+// // sends the request
+// req.end();
