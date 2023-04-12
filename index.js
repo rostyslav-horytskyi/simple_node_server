e97820ec48c6ca96f9286a305a041d40af318c03
Rostyslav Horytskyi <gorzer.rostyslav@gmail.com>
2023-04-12 11:47:51 +0300

    create secure static server

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const PORT = process.env.PORT || 3000;
 
 // Create server
 const server = http.createServer((req, res) => {
-  const fileName = req.url.slice(1) || "index.html";
+  const normalizedURL = new url.URL(req.url, `http://${req.headers.host}`);
+  const fileName = normalizedURL.pathname.slice(1) || "index.html";
 
   fs.readFile(`./public/${fileName}`, (err, data) => {
     if (!err) {
